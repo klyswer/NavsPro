003b4f1ec59fb815a2f441aad3fa032e512ef091
klyswer <klyswer@gmail.com>
2020-04-22 04:48:18 -0400

    Updates

---
+++ precache-manifest.99b218fc10a7517467375f9599fb774c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f400d0f02e6681e0d7aac1e23470d675",
+    "url": "/klyswer/Mpr-r.git/index.html"
+  },
+  {
+    "revision": "37f9ea659d91b7a80cf9",
+    "url": "/klyswer/Mpr-r.git/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c6adb3eb9ba9d7dd9d19",
+    "url": "/klyswer/Mpr-r.git/static/css/main.05cdcc21.chunk.css"
+  },
+  {
+    "revision": "37f9ea659d91b7a80cf9",
+    "url": "/klyswer/Mpr-r.git/static/js/2.e28ec89f.chunk.js"
+  },
+  {
+    "revision": "b58d8a6c0e5872b60955ce6fbfbe23c2",
+    "url": "/klyswer/Mpr-r.git/static/js/2.e28ec89f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6adb3eb9ba9d7dd9d19",
+    "url": "/klyswer/Mpr-r.git/static/js/main.accb73f8.chunk.js"
+  },
+  {
+    "revision": "a2f872122d79e8614669",
+    "url": "/klyswer/Mpr-r.git/static/js/runtime-main.4e1d05b3.js"
+  }
+]);
